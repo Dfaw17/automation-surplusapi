9c5d9d6999b5dfcd45c1ece46b628dc67f4658f8
Dfaw17 <dfawwaz17@gmail.com>
2021-11-22 15:12:02 +0700

    Integration test - test

--- main.py
+++ main.py
@@ -9,11 +9,4 @@ from datetime import *
 sys.path.append('.../IntegrationTest')
 
 # ================================================================================================
-param = {
-    'order_number': settings.var_insert_review().json().get('data')['registrasi_order_number'],
-    'rating': '5',
-    'review': 'ok',
-    'is_hide_name': '3'
-}
-insert_review = requests.post(settings.url_insert_review_customer,params=param, headers=settings.header_with_token_customer)
-print(insert_review.json())
+print('')
