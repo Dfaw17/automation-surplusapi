1f71e26df6441219f7521b46be41420a59da0b83
Dfaw17 <dfawwaz17@gmail.com>
2022-02-14 09:57:53 +0700

    Integration test - test report wrong

--- 1Merchant/Auth/test_logout.py
+++ 1Merchant/Auth/test_logout.py
@@ -26,7 +26,7 @@ class TestLogout:
 
         assert validate_status == bool(True)
         assert response.status_code == 200
-        assert validate_message == "User logged out successfully"
+        assert validate_message == "User logged out successfullys"
 
     def test_logout_wrong_token(self):
         param = {
