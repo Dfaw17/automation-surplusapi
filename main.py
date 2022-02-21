067f992b102282b3efd3df1933e048044a7a56cf
Dfaw17 <dfawwaz17@gmail.com>
2022-02-21 13:51:04 +0700

    Integration test - Customer - adjustment report

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import sys
-import requests
-import settings
-import time
-from assertpy import assert_that
-
-sys.path.append('.../IntegrationTest')
-
-review = requests.post(settings.url_like_unlike_review_customer + str(
-    settings.var_index_review().json().get('data')['reviews'][0]['id']) + '/like-dislike',
-                       headers=settings.header_with_token_customer)
-print(review.json())
+print('Hi Pycharm!')
